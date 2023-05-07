36b0be8a8306e2ea826b0d7ae259e4ee672478b8
Richard Fry <RichardFry@users.noreply.github.com>
2023-05-07 12:30:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express'); // import express
-const app = express();  // initialize express
-// GET route
-app.get('/', function(req, res) {
-  res.send({'hello': 'world'})
-})
+const app = express(); // initialize express
+
 // Listen on port 3000
 app.listen(3000, function() {
   console.log('listening on port 3000...');
