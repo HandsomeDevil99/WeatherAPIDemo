286c5c94bfcc0e60f48b0a5230423b4ec79482ef
Richard Fry <RichardFry@users.noreply.github.com>
2023-05-15 11:13:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const express = require('express'); // import express
 const app = express(); // initialize express
-const fakeWeatherData = require('./data.js'); // import fake data
-// GET route
-app.get('/', function(req, res) {
-  res.send(fakeWeatherData[0]);
-})
-
-
 
 // Listen on port 3000
 app.listen(3000, function() {
