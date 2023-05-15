c49dc209a4c71904a8b41035a76eb621bfa1e54f
Alex <112414349+HandsomeDevil99@users.noreply.github.com>
2023-05-15 13:08:46 -0600

    Initial

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express'); // import express
 const app = express(); // initialize express
+//GET route
+app.get('/weather',function(req,res){
+//store query string parameter in CityName variable
+//res.send("Hello world");
+let cityName = req.query.city.toLowerCase();
+for(let i =0; i<fakeWeatherData.length;i++){
 
+
+  //if no city parameter exists
+  if(!req.query.city){
+    res.send({"status":"error","message":"This city does not exist"});
+   // res.send
+  }
+  else if(cityName == fakeWeatherData[i].city.toLowerCase()){
+    res.send(fakeWeatherData[i]);
+  }
+}
+});
 // Listen on port 3000
 app.listen(3000, function() {
   console.log('listening on port 3000...');
